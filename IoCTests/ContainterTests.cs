894d03926fe91e295d14b83d59c6f0d41cb67c91
tornikegomareli <gomareli@itstep.org>
2018-04-03 23:56:25 +0400

    creates an instance with params and creates an instance with no params

--- IoCTests/ContainterTests.cs
+++ IoCTests/ContainterTests.cs
@@ -32,16 +32,35 @@ namespace IoCTests
     public class Container_GetInstance : ContainerTestBase
     {
         [Test]
-        public void CreatesAnInstanceWithParams()
+        public void CreatesAnInstanceWithNoParams()
         {
             var subject = (A) Container.GetInstance(typeof(A));
 
             Assert.IsInstanceOf<A>(subject);
         }
 
-        class A
+
+        [Test]
+        public void CreatesAnInstanceWithParams()
+        {
+            var subject = (B) Container.GetInstance(typeof(B));
+            
+            Assert.IsInstanceOf<A>(subject.A);
+        }
+        
+        internal class A
         {
             
+        }
+
+        internal class B
+        {
+            public A A { get; set; }
+
+            public B(A a)
+            {
+                A = a;
+            }
         }
     }
 }
